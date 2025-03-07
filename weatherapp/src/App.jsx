9e59d72f16b4e8e793574fe557d42b440c93f643
silverwing2024 <baji4742@gmail.com>
2025-03-07 19:21:16 +0900

    feat: 20250307 react query lesson

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,12 +1,46 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   
-  return (
-    <>
-      
-    </>
-  )
+
+  const [weather, setWeather] = useState({
+    temp:'',
+    desc:'',
+    icon: '',
+  });
+
+  useEffect(() => {
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&APIkey=df28201edf09514c69660c48696626ff')
+    .then(response => response.json())
+    .then(result => {
+
+      setWeather({
+        temp: result.main.temp,
+        desc: result.weather[0].description,
+        icon: result.weather[0].icon,
+      });
+
+    })
+    .catch(error => console.log(error))
+
+  },[]);
+
+  if(weather.icon){
+    return (
+      <>
+        <p>온도: {weather.temp} 도</p>
+        <p>설명: {weather.desc}</p>
+        <img src = {`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="날씨 아이콘"></img>
+      </>
+    )
+  }
+  else{
+    return <div>로딩 중...</div>
+  }
+ 
 }
 
 export default App
+
