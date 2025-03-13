ff4c6c0d44a812f19b0f73fc8662ffac9bce202a
silverwing2024 <baji4742@gmail.com>
2025-03-14 00:19:40 +0900

    feat: 20250313 useState review

--- 01-starting-project/src/App3.jsx
+++ 01-starting-project/src/App3.jsx
@@ -2,17 +2,8 @@ import Header2 from "./Components/Header/Header2";
 import CoreConcepts from "./Components/CoreConcepts/CoreConcepts.jsx";
 import {CORE_CONCEPTS} from "./data.js"
 import TabButton from "./Components/TabButton.jsx";
-import { useState } from "react";
 
 function App() {
-  
-  const [ selectedTopic, setSelectedTopic ] = useState('');
-  
-  function handleSelect(selectedTopic){
-    //selectedButton = 'component' , 'jsx', 'props', 'state'
-    console.log(selectedTopic)
-  }
-
   return (
     <div>
       <Header2 />
@@ -33,11 +24,14 @@ function App() {
         <section id="examples">
           <h2>예시들</h2>
           <menu>
-            <TabButton onSelect={handleSelect('굿나잇')}>Components</TabButton>
+            {/* <TabButton>Components</TabButton>
             <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
-
+            <TabButton>blabla</TabButton>
+            <TabButton>.....</TabButton> */}
+            <TabButton label='Components' />
+            <TabButton label='JSX' />
+            <TabButton label='Props' />
+            <TabButton label='State' />
           </menu>
           동적인 버튼 클릭의 결과물
         </section>
