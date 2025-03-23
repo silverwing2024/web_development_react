aac16a7e7c12f8909145072dfc8da9287bc844f6
silverwing2024 <baji4742@gmail.com>
2025-03-23 22:12:12 +0900

    feat: 20250321 spread syntax / .map() method / todolist

--- reacttodoapp/src/App.jsx
+++ reacttodoapp/src/App.jsx
@@ -1,12 +1,40 @@
-
+import { useState } from "react";
+import './App.css';
 
 function App() {
-  
+  const [toDo, setTodo] = useState('');
+  const [toDos, setTodos] = useState([]);
 
+  const onChange = (event) => {
+  setTodo(event.target.value)    
+  };
+
+  const onSubmit = (event) => {    
+    event.preventDefault();
+    
+    if(toDo===''){
+      return;
+    }   
+    setTodos((currentArray) => [toDo,...currentArray]); 
+    setTodo('');         
+  }
+  
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>My To Dos</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          onChange={onChange}
+          value={toDo}
+          placeholder="Type your To Do 💻"/>
+        <button>Add to do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}        
+      </ul>
+    </div>
   )
 }
 
