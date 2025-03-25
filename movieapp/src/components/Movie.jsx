313e82eeb3edafbea93d3b0e1660796671ca1fea
silverwing2024 <baji4742@gmail.com>
2025-03-25 19:25:45 +0900

    feat: project 안내

--- movieapp/src/components/Movie.jsx
+++ movieapp/src/components/Movie.jsx
@@ -17,17 +17,4 @@ return(
 }
 export default Movie;
 
-// function Movie({ key, coverImg, title, summary, genres }) {
-//   return (
-//     <div key={key}>
-//       <img src={coverImg} alt={title} />
-//       <h2>{title}</h2>
-//       <p>{summary}</p>
-//       <ul>
-//         {genres.map((genre) => (<li key={genre}>{genre}</li>))}
-//       </ul>
-//     </div>
-//   )
-// }
 
-// export default Movie;
